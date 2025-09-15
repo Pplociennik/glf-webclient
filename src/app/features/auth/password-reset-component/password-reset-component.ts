5c63abf4fb4c550274b04cafaa3de981f602c131
pplociennik <pplociennik097@gmail.com>
2025-09-15 04:23:28 +0200

    Improvements in the password reset view

--- src/app/features/auth/password-reset-component/password-reset-component.ts
+++ src/app/features/auth/password-reset-component/password-reset-component.ts
@@ -1,17 +1,23 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-password-reset-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './password-reset-component.html',
   styleUrl: './password-reset-component.scss'
 })
 export class PasswordResetComponent {
   @Input() email!: string;
   requestSent: boolean = false;
+  isInputEmpty: boolean = true;
 
   sendResetLink() {
     this.requestSent = true;
+  }
+
+  checkInput() {
+    this.isInputEmpty = !this.email;
   }
 }
